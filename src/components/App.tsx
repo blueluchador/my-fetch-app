b2aca8d050d49ee2d738f5f394fe5270a118108a
Gerry Gastelum <gerry.gastelum.g@gmail.com>
2024-09-24 21:25:26 -0600

    Refactor

---
+++ src/components/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Pane, Text } from "evergreen-ui";
+
+export default function App() {
+  return (
+    <Pane>
+      <Text>Hello world!</Text>
+    </Pane>
+  );
+}
