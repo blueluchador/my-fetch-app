6d73cd6713d6a4848988377ef0fbf2598102b27c
Gerry Gastelum <gerry.gastelum.g@gmail.com>
2024-09-26 17:18:21 -0600

    Added redux to the project

--- src/index.tsx
+++ src/index.tsx
@@ -2,19 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import { IntlProvider } from "react-intl";
+import { Provider } from "react-redux";
 
 import App from "./components/App";
 import { messages } from "./locales";
+import { store } from "./redux";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <IntlProvider defaultLocale="en-US" locale="en-US" messages={messages["en-US"]}>
-      <HelmetProvider>
-        <App />
-      </HelmetProvider>
-    </IntlProvider>
+    <Provider store={store}>
+      <IntlProvider defaultLocale="en-US" locale="en-US" messages={messages["en-US"]}>
+        <HelmetProvider>
+          <App />
+        </HelmetProvider>
+      </IntlProvider>
+    </Provider>
   </React.StrictMode>,
 );
 
