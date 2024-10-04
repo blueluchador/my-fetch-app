5e5e78f10faa85f0e3867dfb1ae8fbb57c83329d
Gerry Gastelum <gerry.gastelum.g@gmail.com>
2024-10-04 09:08:07 -0600

    Created dog match thunks

--- src/redux/actions/index.ts
+++ src/redux/actions/index.ts
@@ -1,3 +1,4 @@
 export * from "./auth.actions";
 export * from "./dogs.actions";
 export * from "./favorites.actions";
+export * from "./matchDog.actions";
---
+++ src/redux/thunks/matchDog.thunks.ts
@@ -0,0 +1,21 @@
+import { ThunkAction } from "redux-thunk";
+
+import { dogMatchApi } from "../../api";
+import { DogMatch } from "../../models";
+import { MatchDogActionTypes, matchDogFailure, matchDogRequest, matchDogSuccess } from "../actions";
+import { RootState } from "../store";
+
+export const fetchDogMatch = (
+  dogs: string[],
+): ThunkAction<Promise<void>, RootState, unknown, MatchDogActionTypes> => {
+  return async (dispatch) => {
+    dispatch(matchDogRequest());
+
+    try {
+      const match: DogMatch = await dogMatchApi(dogs);
+      matchDogSuccess(match);
+    } catch (error) {
+      dispatch(matchDogFailure((error as Error).message));
+    }
+  };
+};
